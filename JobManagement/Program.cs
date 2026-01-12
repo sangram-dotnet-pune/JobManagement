59fbe62c81b3944e3011577d4c2140e6da03e45f
Jayanth Bandi <bandijayanthh@gmail.com>
2026-01-12 14:10:34 +0530

    Added the Controller

--- JobManagement/Program.cs
+++ JobManagement/Program.cs
@@ -12,12 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.Configure<JwtSettings>(
-    builder.Configuration.GetSection("JwtSettings")
-);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<JobManagementDbContext>();
+builder.Services.AddDbContext<JobManagementDbContext>(Options=>Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IJobRepository, JobRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
